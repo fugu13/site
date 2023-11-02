4ef67c783abdc650bcc7d9bdccb075a185738d81
Russell Duhon <fugu13@gmail.com>
2023-11-02 15:05:39 -0700

    switch to export-only main

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod templates;
 
 use perseus::prelude::*;
 
-#[perseus::main(perseus_axum::dflt_server)]
+#[perseus::main_export]
 pub fn main<G: Html>() -> PerseusApp<G> {
     PerseusApp::new()
         .template(crate::templates::index::get_template())
